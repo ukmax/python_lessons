f86df921f3d63ffe3816bfa1010e49e7119acccb
ukmax <tealiss@ya.ru>
2017-12-24 22:34:54 +0300

    test_add_group.py переведен на pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
     #тест1 - создание группы
-    def test_add_group(self):
-        success = True
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="my_group", header="headertext", footer="footertext"))
-        self.app.logout(success)
+def test_add_group(app):
+    success = True
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="my_group", header="headertext", footer="footertext"))
+    app.logout(success)
 
     # тест2 - создание пустой группы
-    def test_add_empty_group(self):
-        success = True
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout(success)
+def test_add_empty_group(app):
+    success = True
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout(success)
 
-    def tearDown(self):
-        self.app.destroy()
