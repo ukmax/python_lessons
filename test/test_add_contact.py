5b23e74345a8b8b2cd76bfe02c830547554c6d5d
ukmax <tealiss@ya.ru>
2018-01-22 00:56:21 +0300

    Задание 11 готов тест test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,12 +5,20 @@ from model.contact import Contact
 # тест1 - создаем контакт и заполняем основные поля
 def test_add_contact(app):
     app.open_home_page()
-    app.contact.create_contact(Contact(firstname="Alex", middlename="Serge", lastname="Pushkin", address="SPB", home="111", mobile="222", work="333", email="sasha@push.qq"))
+    old_contacts = app.contact.get_contact_list()
+    #cont = Contact(firstname="F0", middlename="M0", lastname="L0", address="SPB", home="111", mobile="222", work="333", email="sasha@push.qq")
+    cont = Contact(firstname="F0", lastname="L0")
+    app.contact.create_contact(cont)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    #old_contacts.append(cont)
+    old_contacts.append(Contact(firstname="L0 F0"))
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     app.open_home_page()
 
 
-# тест2 - создаем контакт с пустыми полями
-def test_add_empty_contact(app):
-    app.open_home_page()
-    app.contact.create_contact(Contact(firstname="", middlename="", lastname="", address="", home="", mobile="", work="", email=""))
-    app.open_home_page()
+# # тест2 - создаем контакт с пустыми полями
+# def test_add_empty_contact(app):
+#     app.open_home_page()
+#     app.contact.create_contact(Contact(firstname="", middlename="", lastname="", address="", home="", mobile="", work="", email=""))
+#     app.open_home_page()
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -7,12 +7,9 @@ def test_del_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(firstname="Alex", middlename="Serge", lastname="Pushkin"))
     old_contacts = app.contact.get_contact_list()
-    print(old_contacts)
     app.contact.delete_first_contact()
     new_contacts = app.contact.get_contact_list()
-    print(new_contacts)
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []
-    print(old_contacts)
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     app.open_home_page()
