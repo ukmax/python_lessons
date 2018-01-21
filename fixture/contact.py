77ea19a621bf468805324f49386a35591db8a418
ukmax <tealiss@ya.ru>
2018-01-21 23:08:15 +0300

    Задание 11 готов тест test_del_first_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,4 @@
+from model.contact import Contact
 
 
 class ContactHelper:
@@ -62,3 +63,13 @@ class ContactHelper:
     def count(self):
         wd = self.app.wd
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_contact_list(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        contacts = []
+        for element in wd.find_elements_by_name("entry"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            contacts.append(Contact(firstname=text, id=id))
+        return contacts
--- model/contact.py
+++ model/contact.py
@@ -1,6 +1,7 @@
+from sys import maxsize
 
 class Contact:
-    def __init__(self, firstname=None, middlename=None, lastname=None, address=None, home=None, mobile=None, work=None, email=None, nickname=None, title=None, company=None, fax=None, email2=None, email3=None, homepage=None, address2=None, phone2=None, notes=None):
+    def __init__(self, id=None, firstname=None, middlename=None, lastname=None, address=None, home=None, mobile=None, work=None, email=None, nickname=None, title=None, company=None, fax=None, email2=None, email3=None, homepage=None, address2=None, phone2=None, notes=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -19,3 +20,16 @@ class Contact:
         self.address2 = address2
         self.phone2 = phone2
         self.notes = notes
+        self.id = id
+
+    def __repr__(self):
+        return "%s,%s,%s,%s,%s" % (self.id, self.firstname, self.middlename, self.lastname, self.mobile)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
