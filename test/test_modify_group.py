0b43678027e66facabe9a1e68c3cf28abd43241a
ukmax <tealiss@ya.ru>
2018-01-22 02:27:49 +0300

    Задание №11: Реализовать проверки в виде сравнения списков

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,11 +4,11 @@ from model.group import Group
 
 # тест - редактируем имя в группе
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
     group = Group(name="new name")
     group.id = old_groups[0].id
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
     app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
