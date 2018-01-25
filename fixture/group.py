292e52bed3aa7c3ca46bc4eaf3340afe51fc1af5
ukmax <tealiss@ya.ru>
2018-01-26 01:40:22 +0300

    Задание №13 - сделано редактирование случайной группы

--- fixture/group.py
+++ fixture/group.py
@@ -31,11 +31,14 @@ class GroupHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def modify_first_group(self, new_group_data):
+        self.modify_group_by_index(0, new_group_data)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         # открываем страницу со списком групп
         self.open_groups_page()
         #выбираем первую группу
-        self.select_first_group()
+        self.select_group_by_index(index)
         # нажимаем кнопку Edit
         wd.find_element_by_name("edit").click()
         # заполняем поля формы новыми значениями
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 
 # тест - редактируем имя в группе
@@ -7,12 +8,13 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="new name")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
