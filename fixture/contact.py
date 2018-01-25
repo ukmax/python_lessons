6703f55dc38ab50510c55d7638edba21136232a6
ukmax <tealiss@ya.ru>
2018-01-25 03:01:56 +0300

    Задание №12: Реализовать кеширование при загрузке списков групп и контактов - готово

--- fixture/contact.py
+++ fixture/contact.py
@@ -15,6 +15,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         # подтверждение сохранения
         wd.find_element_by_name("submit").click()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -22,6 +23,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def modify_first_contact (self, new_contact_data):
         wd = self.app.wd
@@ -32,6 +34,7 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         #нажимаем кнопку update
         wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+        self.contact_cache = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -64,16 +67,19 @@ class ContactHelper:
         wd = self.app.wd
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.app.open_home_page()
-        contacts = []
-        rows = wd.find_elements_by_name("entry")
-        print(rows)
-        for element in rows:
-            cells = element.find_elements_by_tag_name("td")
-            lastname_text = cells[1].text
-            firstname_text = cells[2].text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(id=id, lastname=lastname_text, firstname=firstname_text))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            rows = wd.find_elements_by_name("entry")
+            print(rows)
+            for element in rows:
+                cells = element.find_elements_by_tag_name("td")
+                lastname_text = cells[1].text
+                firstname_text = cells[2].text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(id=id, lastname=lastname_text, firstname=firstname_text))
+        return list(self.contact_cache)
