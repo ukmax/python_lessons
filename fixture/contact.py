a3ae7aa43f58f9dcbffb2960bded2faed8840e60
ukmax <tealiss@ya.ru>
2018-01-28 04:51:55 +0300

    урок 5.5 - готово

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -118,3 +119,14 @@ class ContactHelper:
         phone2 = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(id=id, lastname=lastname, firstname=firstname,
                        home=home, mobile=mobile, work=work, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home=home, mobile=mobile, work=work, phone2=phone2)
+
