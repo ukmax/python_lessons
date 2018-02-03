b7359a92c39132f8e5f0a51ec38105e0bef990ac
ukmax <tealiss@ya.ru>
2018-02-03 19:36:21 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов - готово

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-# тест1 - создаем контакт и заполняем основные поля
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(maxlen):
+    symbols = string.digits + "-" + "(" + ")"
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(lastname="", firstname="", address="", email="", home="", mobile="", work="", phone2="")]\
+           + [Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 10),
+                      address=random_string("address", 10), email=random_string("email", 10),
+                      home=random_number(10), mobile=random_number(10), work=random_number(10),
+                      phone2=random_number(10))
+              for i in range(3)]
+
+
+@pytest.mark.parametrize("cont", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, cont):
     app.open_home_page()
     old_contacts = app.contact.get_contact_list()
-    cont = Contact(lastname="L0", firstname="F0", address="SPB", email="some_email",
-                   home="111", mobile="222", work="333", phone2="444")
     app.contact.create_contact(cont)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
-
     old_contacts.append(cont)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     app.open_home_page()
-
-
-# # # тест2 - создаем контакт с пустыми полями
-# def test_add_empty_contact(app):
-#     app.open_home_page()
-#     old_contacts = app.contact.get_contact_list()
-#     cont = Contact(lastname="", firstname="", address="", email="",
-#                    home="", mobile="", work="", phone2="")
-#     app.contact.create_contact(cont)
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == app.contact.count()
-#     old_contacts.append(cont)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-#     app.open_home_page()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,7 +12,7 @@ def random_string(prefix, maxlen):
 
 testdata = [Group(name="", header="", footer="")]+[
     Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-    for i in range(10)
+    for i in range(3)
 ]
 
 
