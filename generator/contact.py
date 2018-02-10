f8355909316c969ededee45da65e8bc392b4656c
ukmax <tealiss@ya.ru>
2018-02-10 20:18:24 +0300

    Задание №18: Реализовать генераторы тестовых данных - готово

---
+++ generator/contact.py
@@ -0,0 +1,47 @@
+from python_lessons.model.contact import Contact
+import random
+import string
+import jsonpickle
+import os.path
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contact.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(maxlen):
+    symbols = string.digits + "-" + "(" + ")"
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(lastname="", firstname="", address="", email="", home="", mobile="", work="", phone2="")] \
+           + [Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 10),
+                      address=random_string("address", 10), email=random_string("email", 10),
+                      home=random_number(10), mobile=random_number(10), work=random_number(10),
+                      phone2=random_number(10))
+              for i in range(n)]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as f:
+    jsonpickle.set_encoder_options("json", indent=2)
+    f.write(jsonpickle.encode(testdata))
