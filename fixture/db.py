14fb82258c5e32b160730c7bfdc92062893f197e
ukmax <tealiss@ya.ru>
2018-02-18 20:28:29 +0300

    7.7 ORM - Ошибка: поле deprecated из БД читается как str, а ожидается datetime

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,7 @@
 import pymysql.cursors
 import mysql.connector
 from python_lessons.model.group import Group
+from python_lessons.model.contact import Contact
 
 
 class DbFixture:
@@ -23,9 +24,24 @@ class DbFixture:
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
+                # в переменные присваиваем значения из кортежа, который соответствует строке
                 (id, name, header, footer) = row
                 # делаем приведение типов для id, т.к. в БД id это int, а из web читаем как str
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, lastname, firstname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                # в переменные присваиваем значения из кортежа, который соответствует строке
+                (id, lastname, firstname) = row
+                # делаем приведение типов для id, т.к. в БД id это int, а из web читаем как str
+                list.append(Contact(id=str(id), lastname=lastname, firstname=firstname))
         finally:
             cursor.close()
         return list
---
+++ fixture/orm.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from pony.orm import *
+from python_lessons.model.group import Group
+from python_lessons.model.contact import Contact
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
+
+
+class ORMFixture:
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        lastname = Optional(str, column='lastname')
+        firstname = Optional(str, column='firstname')
+        deprecated = Optional(datetime, column='deprecated')
+
+    def __init__(self, host, name, user, password):
+        # при использовании Pony данные из БД могут преобразовываться для использования в объектах
+        # conv - берем списки преобразователей типов из PyMySQL, а не по умолчанию из Pony
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=conv)
+        self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+
+        return list(map(convert, groups))
+
+    @db_session
+    def get_group_list(self):
+        # выводим все записи из _table_ = 'group_list'
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), lastname=contact.lastname, firstname=contact.lastname)
+
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        # выводим записи из _table_ = 'addressbook', у которых пустое поле deprecated, хотя в БД там нули
+        # нулевые даты из  БД преобразуются в None, т.к. используется преобразователь из PyMySQL
+        # преобразователь определен в self.db.bind()
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
