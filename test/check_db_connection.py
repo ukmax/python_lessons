2ad24dc1ab31723e2e0f40260f792ddb479a2970
ukmax <tealiss@ya.ru>
2018-02-21 02:17:25 +0300

    7.8 выбор контактов, принадлежащих группе db.get_contacts_in_group(Group(id='337'))

--- test/check_db_connection.py
+++ test/check_db_connection.py
@@ -1,9 +1,10 @@
 from python_lessons.fixture.orm import ORMFixture
+from python_lessons.model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id='337'))
     for item in l:
         print(item)
     print(len(l))
